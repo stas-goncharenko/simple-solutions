482d0f461b21ba376ba48954b7c5754ef0c38664
Stas Goncharenko <goncharenko.stas@i.ua>
2015-04-08 00:24:26 +0300

    Create phpArrayToUrlAndJsGetArrayFromUrl.php

---
+++ phpArrayToUrlAndJsGetArrayFromUrl.php
@@ -0,0 +1,16 @@
+<?php
+$params = array(
+    'param1' => array(
+        0 => 'aaa',
+        1 => 'bbb',
+    ),
+    'param2' => array(
+        0 => 'ccc',
+        1 => 'ddd',
+    )
+);
+$params = json_encode($params);
+$params = base64_encode($params);
+$params = urlencode($params);
+
+header('Location: ' . 'http://example.loc/phpArrayToUrlAndJsGetArrayFromUrl.html?params=' . $params);
